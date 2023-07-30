72606328dfc44671c5c82a4f2a0220183fbad943
Isaac <78666088+IPRIESPM@users.noreply.github.com>
2023-07-30 11:31:28 +0200

    feat: :sparkles: add logic of JWT

---
+++ src/jwt/jwt.js
@@ -0,0 +1,31 @@
+const jws = require('jsonwebtoken');
+require('dotenv').config();
+
+function generateToken(email, nombre) {
+  const payload = { email, nombre };
+  const options = { expiresIn: '6d' };
+
+  const token = jws.sign(payload, process.env.JWT_SECRET, options);
+  return token;
+}
+
+function verifyToken(req, res, next) {
+  // Pedimos el token en el header de la petición
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).send({ error: 'No token provided' });
+
+  try {
+    // Comprobamos que el token es válido
+    const decoded = jws.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    // Pasamos al siguiente middleware
+    return next();
+  } catch (err) {
+    return res.status(401).send({ error: 'Invalid token' });
+  }
+}
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
